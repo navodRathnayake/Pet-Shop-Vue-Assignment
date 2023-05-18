9bc370de0c834e8a2d7d76ae429bb50b07844246
Navod Rathnayake <82445185+navodRathnayake@users.noreply.github.com>
2023-05-18 18:32:23 +0530

    commitment

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,15 @@ import HomeComponent from './components/HomeComponent.vue'
 import AboutUs from './components/AboutUSComponent.vue'
 import RescueBanner from './components/RescueBannerComponent.vue'
 import ContactUs from './components/ContactUsComponent.vue'
+import AppFooter from './components/AppFooterComponent.vue'
 </script>
 
 <template>
   <HomeComponent />
-  <!-- <RouterView /> -->
   <AboutUs />
   <RescueBanner />
   <ContactUs />
+  <AppFooter />
 </template>
 
 <style scoped>
