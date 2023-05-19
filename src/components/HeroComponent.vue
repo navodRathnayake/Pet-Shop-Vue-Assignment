ade7fc6cde37f4b4a0577e2172b7320fee979120
Navod Rathnayake <82445185+navodRathnayake@users.noreply.github.com>
2023-05-19 08:44:45 +0530

    added ubuntu font

--- src/components/HeroComponent.vue
+++ src/components/HeroComponent.vue
@@ -25,7 +25,7 @@
 <template>
   <div class="bg-white relative pt-40 pb-20 lg:pt-44 dark:bg-gray-900">
     <div class="relative xl:container m-auto px-6 md:px-12 lg:px-6">
-        <h1 class="sm:mx-auto sm:w-10/12 md:w-2/3 font-black text-blue-900 text-4xl text-center sm:text-5xl md:text-6xl lg:w-auto lg:text-left xl:text-7xl dark:text-white">Run successful remote and <br class="lg:block hidden"> <span class="relative text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-cyan-500 dark:from-blue-400 dark:to-cyan-300">Hybrid teams</span>.</h1>
+        <h1 class="sm:mx-auto sm:w-10/12 md:w-2/3 font-black text-blue-900 text-4xl text-center sm:text-5xl md:text-6xl lg:w-auto lg:text-left xl:text-7xl dark:text-white">We Are Always Here For <br class="lg:block hidden">All Your Pet's <br class="lg:block hidden">Good Health.</h1>
         <div class="lg:flex">
             <div class="relative mt-8 md:mt-16 space-y-8 sm:w-10/12 md:w-2/3 lg:ml-0 sm:mx-auto text-center lg:text-left lg:mr-auto lg:w-7/12">
                 <p class="sm:text-lg text-gray-700 dark:text-gray-300 lg:w-11/12">
