dbc655c8ede510b032a873ddc0d2633855ba5aea
Navod Rathnayake <82445185+navodRathnayake@users.noreply.github.com>
2023-05-25 21:33:25 +0530

    added marketplace logic

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -2,9 +2,10 @@
 export default {
   name : 'Cart',
   props : {
+    img : String,
     title : String,
     price : String,
-    quentity : String
+    type : String,
   }
 };
 </script>
@@ -12,7 +13,7 @@ export default {
 <template>
     <div class="relative max-w-sm min-w-[340px] bg-white shadow-md rounded-3xl p-1 mx-1 my-3 cursor-pointer">
         <div class="overflow-x-hidden rounded-2xl relative">
-          <img class="h-40 rounded-2xl w-full object-cover" src="https://pixahive.com/wp-content/uploads/2020/10/Gym-shoes-153180-pixahive.jpg">
+          <img v-bind:src="img" class="h-40 rounded-2xl w-full object-cover"  />
           <p class="absolute right-2 top-2 bg-white rounded-full p-2 cursor-pointer group">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 group-hover:opacity-50 opacity-70" fill="none" viewBox="0 0 24 24" stroke="black">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
@@ -25,9 +26,12 @@ export default {
             <p class="text-md text-gray-800 mt-0">{{price}}</p>
           </div>
           <div class="flex flex-col-reverse mb-1 mr-4 group cursor-pointer">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 group-hover:opacity-70" fill="none" viewBox="0 0 24 24" stroke="gray">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-            </svg>
+            <div v-if = "type === 'food'">
+              <font-awesome-icon :icon="['fas', 'bowl-food']" />
+            </div>
+            <div v-if = "type === 'medicine'">
+              <font-awesome-icon :icon="['fas', 'prescription-bottle-medical']" />
+            </div>
           </div>
         </div>
       </div>
--- src/components/ProductCardView.vue
+++ src/components/ProductCardView.vue
@@ -14,11 +14,11 @@ export default {
 </script>
 
 <template>
-<div class="h-[100vh] bg-gray-100 flex flex-col ">
+<div class="h-[100vh] bg-gray-100 overflow-y-auto flex flex-col flex-grow">
     <div class="relative m-3 flex flex-wrap mx-auto justify-center pt-0">
   
-      <div v-for="(item, index) in JsonData.cart.item" :key="index">
-        <Cart :title= "item.title"  :price="item.price" :quantity="item.quantity" />
+      <div v-for="(item, index) in JsonData.product" :key="index">
+        <Cart :img="item.img" :title= "item.title"  :price="item.price" :type="item.type" />
       </div>
   
     </div>
